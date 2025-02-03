53c7efbd718aebc9ef6cd294626dec4e940018f1
developer123 <lo14ubna02@gmail.com>
2025-02-03 23:14:03 +0100

    comments

--- Projjet/Projjet/settings.py
+++ Projjet/Projjet/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'monapp'
+    'monapp',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
