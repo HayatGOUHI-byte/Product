1a9e2774580829375ff9271dcc4eee34b793d2a6
developer123 <lo14ubna02@gmail.com>
2025-02-04 11:22:54 +0100

    adding the First paths

--- Projjet/Projjet/settings.py
+++ Projjet/Projjet/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'monapp',
-    'bootstrap4',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
--- Projjet/monapp/views.py
+++ Projjet/monapp/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.urls import reverse_lazy
+
+from.models import Produit
+
+
+#Afficher tous les produits
+
+class ProduitListView(ListView):
+	model = Produit
+	template_name = 'produit_list.html'
+	context_object_name = 'produits'
+
+
+#ajouter un produit
+class ProduitCreateView(createView):
+	model = Produit 
+	template_name = 'produit_form.html'
+	fileds = ['nom','prix']
+	sucess_url = reverse_lazy('produit-list')
+
+
+#supprimer un produit
+class ProduitDeleteView(DelteView):
+	model = Produit 
+	template_name = 'produit_confirm_delete.html'
+	success_url = reverse_lazy('produit-list')
+
+#détails d'un produit
+
+class ProduitDetailView(DetailView):
+	model = Produit
+	template_name ='produit_detail.html'
+	context_object_name = 'produit'
