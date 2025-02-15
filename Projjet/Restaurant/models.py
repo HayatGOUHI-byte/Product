884f53239225c8b55312cee0c5895f78b91bef03
developer123 <lo14ubna02@gmail.com>
2025-02-15 17:01:47 +0100

    models added to application

--- Projjet/Restaurant/models.py
+++ Projjet/Restaurant/models.py
@@ -5,3 +5,6 @@ class Menu(models.Model):
 	nom = models.CharField(max_length=100)
 	description = models.TextField()
 	disponible = models.BooleanField(default=True)
+
+	def __str__(self):
+		return self.nom
