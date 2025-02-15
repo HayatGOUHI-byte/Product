1ed23740cbbecfaf5e6180722b2ac33c89b6ba57
developer123 <lo14ubna02@gmail.com>
2025-02-15 21:16:12 +0100

    added files

--- Projjet/Restaurant/views.py
+++ Projjet/Restaurant/views.py
@@ -17,3 +17,17 @@ def menu_detail_view(request, menu_id):
 	menu = get_object_or_404(Menu, id=menu_id)
 	plats = menu.plats.all()
 	return render(request,'restaurant/menu_detail.html',{'menu':menu, 'plats':plats})
+
+
+
+# Passer une commande
+def passer_commande_view(request, plat_id):
+    plat = get_object_or_404(Plat, id=plat_id)
+    if request.method == 'POST':
+        client = get_object_or_404(Client, id=request.POST['client_id'])
+        commande = Commande(client=client)
+        commande.save()
+        commande.plat.add(plat)
+        commande.save()
+        return HttpResponse("Commande passée avec succès!")
+    return render(request, 'restaurant/passer_commande.html', {'plat': plat})
