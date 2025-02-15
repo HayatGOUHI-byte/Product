47cbf52df3c913761d7da57ed1a84d081b69f653
developer123 <lo14ubna02@gmail.com>
2025-02-15 21:14:02 +0100

    added files

--- Projjet/Restaurant/urls.py
+++ Projjet/Restaurant/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
 path('', views.index, name='index'),
 path('menu/', views.menu_view, name='menu_view'),
+path('menu/<int:menu_id>/', views.menu_detail_view, name='menu_detail'),
 
 ]
