b481af9ce69f1dac2c422c53f2a3ab079401ffdb
developer123 <lo14ubna02@gmail.com>
2025-02-15 17:34:55 +0100

    changes here

--- Projjet/Restaurant/models.py
+++ Projjet/Restaurant/models.py
@@ -26,7 +26,7 @@ class Client(models.Model):
 	adresse = models.TextField()
 
 	def __str__(self):
-		return self.no
+		return self.nom
 
 class Commande(models.Model):
 	client = models.ForeignKey(Client, related_name="commandes", on_delete = models.CASCADE)
