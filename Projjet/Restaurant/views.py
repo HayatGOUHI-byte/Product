98a89efa121457f8593e3fb3ba1b1a4f0260b7e3
developer123 <lo14ubna02@gmail.com>
2025-02-15 21:12:32 +0100

    added files

--- Projjet/Restaurant/views.py
+++ Projjet/Restaurant/views.py
@@ -11,3 +11,9 @@ def index(request):
 def menu_view(request):
 	menus = Menu.objects.all()
 	return render(request,'restaurant/menu.html', {'menus':menus})
+
+#menu_detail
+def menu_detail_view(request, menu_id):
+	menu = get_object_or_404(Menu, id=menu_id)
+	plats = menu.plats.all()
+	return render(request,'restaurant/menu_detail.html',{'menu':menu, 'plats':plats})
