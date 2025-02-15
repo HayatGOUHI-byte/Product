0ac28a63212620f1d87c07ab74e092caa436fa01
developer123 <lo14ubna02@gmail.com>
2025-02-15 17:12:00 +0100

    changes here

--- Projjet/Restaurant/models.py
+++ Projjet/Restaurant/models.py
@@ -24,3 +24,6 @@ class Client(models.Model):
 	email = models.EmailField(unique=True)
 	telephone = models.CharField(max_length=20)
 	adresse = models.TextField()
+
+	def __str__(self):
+		return self.nom
