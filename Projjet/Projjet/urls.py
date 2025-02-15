f52e3f64497fbdb7bcad9c61fe5e7ed0d4465200
developer123 <lo14ubna02@gmail.com>
2025-02-15 15:44:29 +0100

    Adding new App Restaurant On django Project

--- Projjet/Projjet/urls.py
+++ Projjet/Projjet/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('monapp.urls')),
+     path('Restaurant/', include('Restaurant.urls')),  # Include new app's Restaurant
 ]
