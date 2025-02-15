f56ef4a451d29fae99d93152a4f5c2e3f214d1d5
developer123 <lo14ubna02@gmail.com>
2025-02-15 17:06:06 +0100

    changes here

--- Projjet/Restaurant/models.py
+++ Projjet/Restaurant/models.py
@@ -14,3 +14,6 @@ class Plat(models.Model):
 	nom = models.CharField(max_length=10)
 	description = models.TextField()
 	prix = models.DecimalField(max_digits=6, decimal_places=2)
+
+	def __str__(self):
+		return self.nom
