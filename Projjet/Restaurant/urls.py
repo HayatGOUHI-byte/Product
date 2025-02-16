139e9e308652e6f33f7bec07f61733915a08f8a9
developer123 <lo14ubna02@gmail.com>
2025-02-16 16:37:09 +0100

    these are the made changes

--- Projjet/Restaurant/urls.py
+++ Projjet/Restaurant/urls.py
@@ -12,5 +12,6 @@ path('menu/', views.menu_view, name='menu_view'),
 path('menu/<int:menu_id>/', views.menu_detail_view, name='menu_detail'),
 path('commande/<int:plat_id>/', views.passer_commande_view, name='passer_commande'),
 path('client/<int:client_id>/commandes/', views.client_commandes_view, name='client_commandes'),
+path('disponible/', views.Menu_dispo, name='Menu_dispo'),
 
 ]
